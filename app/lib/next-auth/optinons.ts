9f2c8ff087eb06b39c6d1cbf70a8e22d852a5cec
raie03 <raie.iwatsuki@gmail.com>
2024-12-21 18:00:10 +0900

    prismaをグローバルインスタンスで定義

--- app/lib/next-auth/optinons.ts
+++ app/lib/next-auth/optinons.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../prisma";
 
 export const nextAuthOptions: NextAuthOptions = {
     debug: false,
