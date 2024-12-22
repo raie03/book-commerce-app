b556a1501ba9f402b145b838bea6e1068fcaa0fd
raie03 <raie.iwatsuki@gmail.com>
2024-12-22 20:09:28 +0900

    デプロイ時エラーの修正

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 export async function GET(
     // request: Request, 
-    {params}:{params: any} ) 
+    {params}:{params: {userId: string}} ) 
     {
         const {userId} = await params;
 
