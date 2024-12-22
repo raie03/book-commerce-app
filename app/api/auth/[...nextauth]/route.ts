18f028ddfee3fa6a2d39ad514ccb429a5b5b905a
raie03 <raie.iwatsuki@gmail.com>
2024-12-22 19:26:03 +0900

    デプロイ時エラーの修正

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,17 @@
 import { nextAuthOptions } from "@/app/lib/next-auth/optinons";
-import { NextApiHandler } from "next";
+// import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 
-const handler: NextApiHandler = NextAuth(nextAuthOptions);
+// const handler: NextApiHandler = NextAuth(nextAuthOptions);
 
-export {handler as GET, handler as POST};
+// export {handler as GET, handler as POST};
+
+type CombineRequest = Request & NextApiRequest;
+type CombineResponse = Response & NextApiResponse;
+
+async function handler(req: CombineRequest, res: CombineResponse) {
+  return await NextAuth(req, res, nextAuthOptions);
+}
+
+export { handler as GET, handler as POST };
