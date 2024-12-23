f51f8592b7c82259ba9f87394ae0f02beea0fe9c
raie03 <raie.iwatsuki@gmail.com>
2024-12-23 17:07:01 +0900

    デプロイ時エラーの修正

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,6 @@
 import { nextAuthOptions } from "@/app/lib/next-auth/optinons";
-//import { NextApiHandler } from "next";
-//import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 
-const handler/*: NextApiHandler*/ = NextAuth(nextAuthOptions);
+const handler = NextAuth(nextAuthOptions);
 
 export {handler as GET, handler as POST};
-
-// type CombineRequest = Request & NextApiRequest;
-// type CombineResponse = Response & NextApiResponse;
-
-// async function handler(req: CombineRequest, res: CombineResponse) {
-//   return await NextAuth(req, res, nextAuthOptions);
-// }
-
-// export { handler as GET, handler as POST };
--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 // import Link from "next/link";
 import { BookType, User } from "../types/types";
 import { useState } from "react";
-import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 type BookProps = {
